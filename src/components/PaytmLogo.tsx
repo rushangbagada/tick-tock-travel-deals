1a7d1e54111648f5ab3bdc40e8b089ec2a4072c7
rushangbagada <rushang697@gmail.com>
2025-05-13 13:10:21 +0530

    size change

--- src/components/PaytmLogo.tsx
+++ src/components/PaytmLogo.tsx
@@ -6,7 +6,7 @@ const PaytmLogo: React.FC = () => {
     <img 
       src="/lovable-uploads/9f4308e5-dbef-4ef5-a6c9-287cfc9cad80.png" 
       alt="Paytm Logo" 
-      className="h-12 w-auto"
+      className="h-[75px] w-auto"
     />
   );
 };
